2024f337f7fc054ca1956ae9cbfb36f02df08f9a
Nicholas Currault <nicholas.curr+dev@gmail.com>
2015-05-25 16:53:44 -0700

    Decided on database things.

---
+++ setup_db.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Vote
+(
+PID SERIAL NOT NULL PRIMARY KEY,
+
+TragHeroImportance INT,
+PeripImportance INT,
+AnagImportance INT,
+SpiralImportance INT,
+
+OkSocial INT,
+OkStubborn INT,
+OkAgressive INT,
+OkHarsh INT,
+OkOther INT,
+
+OkPerip1Count INT,
+OkPerip2Count INT,
+OkPerip3Count INT,
+OkPerip4Count INT,
+
+OkAnag INT,
+OkEffect INT,
+
+OthSocial INT,
+OthFlaw INT,
+OthPerip INT,
+OthAnag INT,
+OthEffect INT,
+
+Weight INT DEFAULT 1
+);
